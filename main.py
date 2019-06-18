252940f40602bd0c74887578f654a752a706ed4a
Phanaty <38048498+Phanaty@users.noreply.github.com>
2019-06-19 01:25:06 +0300

    sql

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ from PyQt5.QtCore import QThread, pyqtSignal
 import ast
 import time
 
+list_friends_buttons = []
+def login_with_sql():
+    global conn, cur
+    try:
+        conn = psycopg2.connect(
+            "dbname='xhbkgwzr' user='xhbkgwzr' host='stampy.db.elephantsql.com' password='54wZQUcm0pw6CY9gXUy7Z4vQ01bP_7ee'")
+        cur = conn.cursor()
+    except psycopg2.OperationalError:
+        msgbox(msg="Отсутствует интернет соединение", title="Login", ok_button="fuck go back")
+
 
 class Loginform(QtWidgets.QMainWindow, login.Ui_Dialog):
     def __init__(self):
@@ -27,7 +37,6 @@ class Loginform(QtWidgets.QMainWindow, login.Ui_Dialog):
         try:
             if (int(self.lineEdit.text().__len__()) == 0) or (int(self.lineEdit_2.text().__len__()) == 0):
                 msgbox(msg="Введите данные", title="Login", ok_button="fuck go back")
-
             else:
                 vk_session = vk_api.VkApi(self.lineEdit.text(), self.lineEdit_2.text())
                 vk_session.auth()
@@ -44,10 +53,32 @@ class Loginform(QtWidgets.QMainWindow, login.Ui_Dialog):
 
 
 class Mainform(QtWidgets.QMainWindow, mainform.Ui_Dialog):
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.load()
+        self.sql()
+
+    def load(self):
+        check = QPushButton('2222' + '1111', self)
+        check.resize(282, 81)
+        check.move(0, 79)
+        # check.clicked.connect(self.choosen_dialog)
+        check.setFont(QtGui.QFont("Times", 12, QtGui.QFont.Bold))
+        check.setStyleSheet('QRadioButton {background-color: #17212b; color: white;}')
+        list_friends_buttons.append(check)
+
+    def sql(self):
+        info = vk.account.getProfileInfo()
+        name = info['first_name']
+        surname = info['last_name']
+        domain = info['screen_name']
+        cur.execute("SELECT * FROM users WHERE DOMAIN = '" + domain + "'")
+        row = cur.fetchone()
+        if not row:
+            cur.execute("INSERT INTO users(domain, name, surname) VALUES (%s,%s,%s)",
+                        (domain, name, surname))  # Добавление информации
+            conn.commit()
 
 
 if __name__ == '__main__':
